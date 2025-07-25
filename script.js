aa7ce9e1f2737881053ccd7610a26b4cdd1cca41
mclarabar <mcbp2@aluno.ifal.edu.br>
2025-07-25 14:37:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let sorteio, contador, resposta2;
+let sorteio, contador;
 
 function sortear(){
     sorteio = Math.floor(Math.random() * 100) + 1;
@@ -6,51 +6,51 @@ function sortear(){
 }
 
 alert('Seja bem-vindo ao Jodo da Adivinhação!');
-alert('Você tem 5 chances para adivinhar o número correto.')
-do{
-    const resposta = prompt('Digite um número de 1 a 100! (Obs.: Caso deseje sair, digite parar)');
+alert('Você tem 5 chances para adivinhar o número correto.');
 
-    if(resposta == 'parar'){
-        alert('Obrigada por participar, volte sempre!');
-        break;
-    }
-        
-        
+do{         
     let numero = sortear();
+    let acertou = false;
+    let resposta;
     contador = 0;
 
     while(contador < 5){
+        if(contador == 0){
+            resposta = prompt('Digite um número de 1 a 100! (Obs.: Caso deseje sair, digite parar)');
+        }else{
+            resposta = prompt('Essa foi a '+contador+'º de 5. Digite o próximo palpite: ');
+        }
+        
+
+        if(resposta == 'parar'){
+          alert('Obrigada por participar, volte sempre!');
+          break;
+        }
+
         let tentativa = parseInt(resposta);
         
         if(isNaN(tentativa) || tentativa < 1 || tentativa > 100){
             alert('Digite um número válido!');
-            break;
+            continue;
         }
             
         contador ++;
 
         if(tentativa == numero){
             alert('Seu palpite ('+ resposta +') está certo, parabéns!');
+            acertou = true;
             break;
-        }else if(resposta < numero){
+        }else if(tentativa < numero){
             alert('Seu palpite ('+ resposta +') está errado, o número certo é maior!');
-        }else{
+        }else if(tentativa > numero){
             alert('Seu palpite ('+ resposta +') está errado, o número certo é menor!');
         } 
-
-        if(contador < 5){
-            resposta2 = prompt('Essa foi a '+contador+'º de 5. Digite o próximo palpite: ').toLowerCase();
-            if (resposta2 == 'parar'){
-                alert('Jogo encerrado, obrigada por participar!');
-                break;
-            }
-            resposta = resposta2;
-        }else{
-            alert('Suas tentativas acabaram! O número era '+numero+'. Até uma próxima!');
-        }
+        
 
     }
-    
 
-} while (true);
+    if(acertou == false && resposta != 'parar') {
+        alert('Suas tentativas acabaram! O número era '+numero+'. Até uma próxima!');
+    } 
 
+} while (resposta != 'parar');
